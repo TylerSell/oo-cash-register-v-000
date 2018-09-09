ff272ed61ea002827f326d30c3fa82b40ac56222
tylerselldesign@gmail.com <tylerselldesign@gmail.com>
2018-09-09 18:37:27 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -20,9 +20,10 @@ class CashRegister
   def apply_discount
     if @discount != 0 
       self.total = (total * ((100.0 - discont.to_f)/100)).to_i
+      
     else 
       @total -= @total * @discount / 100
-      puts "After the disount, the total comes to #{@total}."
+      puts "After the disount, the total comes to #{self.total}."
     end
   end
   
